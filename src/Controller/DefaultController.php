1fffd85de60ef1eb78353b0ded45cda00a34ccaa
ViotSebastien <viot.sebastien@oostaoo.com>
2019-01-21 16:15:31 +0100

    mise à jours de symfony

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+//src/Controller/DefaultController.php
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+
+  public function index()
+  {
+    return new Response('Hello World');
+  }
+}
+
+
+ ?>
